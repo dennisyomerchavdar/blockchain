d87ac98a33497684de5b2ce550a3834423baf521
omer <omercavdar365@gmail.com>
2020-06-11 04:01:06 +0300

    init

---
+++ cmake-build-debug/CMakeFiles/blockchain.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/blockchain.dir/main.cpp.o"
+  "blockchain"
+  "blockchain.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/blockchain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
