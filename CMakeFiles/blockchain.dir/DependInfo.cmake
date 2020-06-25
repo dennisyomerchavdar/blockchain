e42a5bcdeeff5fdf061954f68a820b15e8917fca
omer <omercavdar365@gmail.com>
2020-06-25 21:40:24 +0300

    Added multithreading

---
+++ CMakeFiles/blockchain.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/omer/CLionProjects/blockchain/block.cpp" "/home/omer/CLionProjects/blockchain/CMakeFiles/blockchain.dir/block.cpp.o"
+  "/home/omer/CLionProjects/blockchain/blockchain.cpp" "/home/omer/CLionProjects/blockchain/CMakeFiles/blockchain.dir/blockchain.cpp.o"
+  "/home/omer/CLionProjects/blockchain/hash.cpp" "/home/omer/CLionProjects/blockchain/CMakeFiles/blockchain.dir/hash.cpp.o"
+  "/home/omer/CLionProjects/blockchain/main.cpp" "/home/omer/CLionProjects/blockchain/CMakeFiles/blockchain.dir/main.cpp.o"
+  "/home/omer/CLionProjects/blockchain/miner.cpp" "/home/omer/CLionProjects/blockchain/CMakeFiles/blockchain.dir/miner.cpp.o"
+  "/home/omer/CLionProjects/blockchain/transaction.cpp" "/home/omer/CLionProjects/blockchain/CMakeFiles/blockchain.dir/transaction.cpp.o"
+  "/home/omer/CLionProjects/blockchain/user.cpp" "/home/omer/CLionProjects/blockchain/CMakeFiles/blockchain.dir/user.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/openssl"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/blockchain.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/blockchain.dir/block.cpp.o"
+  "CMakeFiles/blockchain.dir/blockchain.cpp.o"
+  "CMakeFiles/blockchain.dir/hash.cpp.o"
+  "CMakeFiles/blockchain.dir/main.cpp.o"
+  "CMakeFiles/blockchain.dir/miner.cpp.o"
+  "CMakeFiles/blockchain.dir/transaction.cpp.o"
+  "CMakeFiles/blockchain.dir/user.cpp.o"
+  "blockchain"
+  "blockchain.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/blockchain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
